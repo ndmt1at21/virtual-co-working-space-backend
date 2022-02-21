c35e409e6a9b8da9d20e081b17308fc19c2410b4
ndmt1at21 <trminh36@gmail.com>
2022-02-21 23:16:07 +0700

    add check token exists by user id

--- src/components/refreshToken/refreshToken.repository.ts
+++ src/components/refreshToken/refreshToken.repository.ts
@@ -4,8 +4,18 @@ import { RefreshToken } from './refreshToken.entity';
 
 @EntityRepository(RefreshToken)
 export class RefreshTokenRepository extends BaseRepository<RefreshToken> {
-	findByToken(token: string): Promise<RefreshToken | undefined> {
-		return this.findOne({
+	async existsTokenWithUserId(userId: number): Promise<boolean> {
+		const count = await this.count({
+			where: {
+				userId
+			}
+		});
+
+		return count === 1;
+	}
+
+	async findByToken(token: string): Promise<RefreshToken | undefined> {
+		return await this.findOne({
 			where: {
 				token
 			}

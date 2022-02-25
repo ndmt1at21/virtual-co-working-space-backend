473a56d13cb23a73a98498ccaedf816c636a3208
ndmt1at21 <trminh36@gmail.com>
2022-02-25 15:07:48 +0700

    add mail subscriber

--- src/components/mail/mail.subscribe.ts
+++ src/components/mail/mail.subscribe.ts
@@ -1,7 +1,15 @@
 import events from 'events';
+import { mailLogger } from '@components/logger';
+import { ILogger } from '@components/logger/@types/ILogger';
+import { IMailService } from './@types/IMailService';
+import { createMailService } from './mail.factory';
 
-const eventEmitter = new events.EventEmitter();
+const MailSubscriber = (mailService: IMailService, logger: ILogger) => {
+	const eventEmitter = new events.EventEmitter();
 
-eventEmitter.addListener('user registered', user => {});
+	eventEmitter.addListener('user registered', user => {});
 
-eventEmitter.addListener('reset password', user => {});
+	eventEmitter.addListener('reset password', ({ email, resetToken }) => {});
+};
+
+MailSubscriber(createMailService(), mailLogger);

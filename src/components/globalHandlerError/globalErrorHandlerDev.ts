9a60ca46b3555b9fe5b542ee4520c4acb69b0be7
ndmt1at21 <trminh36@gmail.com>
2022-03-05 08:39:30 +0700

    refactor global error handler

--- src/components/globalHandlerError/globalErrorHandlerDev.ts
+++ src/components/globalHandlerError/globalErrorHandlerDev.ts
@@ -1,9 +1,5 @@
 import { ErrorRequestHandler } from 'express';
-import {
-	NotFoundError,
-	IllegalArgumentError,
-	UnauthorizedError
-} from '@src/utils/appError';
+import { AppError } from '@src/utils/appError';
 import { HttpStatusCode } from '@src/constant/httpStatusCode';
 
 export const globalErrorHandlerDev: ErrorRequestHandler = (
@@ -12,28 +8,8 @@ export const globalErrorHandlerDev: ErrorRequestHandler = (
 	res,
 	next
 ) => {
-	if (err instanceof NotFoundError) {
-		res.status(HttpStatusCode.NOT_FOUND).json({
-			message: err.message,
-			stack: err.stack,
-			error: err
-		});
-
-		return;
-	}
-
-	if (err instanceof IllegalArgumentError) {
-		res.status(HttpStatusCode.BAD_REQUEST).json({
-			message: err.message,
-			stack: err.stack,
-			error: err
-		});
-
-		return;
-	}
-
-	if (err instanceof UnauthorizedError) {
-		res.status(HttpStatusCode.UNAUTHORIZED).json({
+	if (err instanceof AppError) {
+		res.status(err.httpCode).json({
 			message: err.message,
 			stack: err.stack,
 			error: err
--- src/components/globalHandlerError/globalErrorHandlerProd.ts
+++ src/components/globalHandlerError/globalErrorHandlerProd.ts
@@ -1,9 +1,5 @@
 import { ErrorRequestHandler } from 'express';
-import {
-	IllegalArgumentError,
-	NotFoundError,
-	UnauthorizedError
-} from '@src/utils/appError';
+import { AppError } from '@src/utils/appError';
 import { HttpStatusCode } from '@src/constant/httpStatusCode';
 
 export const globalErrorHandlerProd: ErrorRequestHandler = (
@@ -12,27 +8,9 @@ export const globalErrorHandlerProd: ErrorRequestHandler = (
 	res,
 	next
 ) => {
-	if (err instanceof NotFoundError) {
-		res.status(HttpStatusCode.NOT_FOUND).json({
+	if (err instanceof AppError) {
+		res.status(err.httpCode).json({
 			message: err.message
-		});
-
-		return;
-	}
-
-	if (err instanceof IllegalArgumentError) {
-		res.status(HttpStatusCode.BAD_REQUEST).json({
-			message: err.message
-		});
-
-		return;
-	}
-
-	if (err instanceof UnauthorizedError) {
-		res.status(HttpStatusCode.UNAUTHORIZED).json({
-			message: err.message,
-			stack: err.stack,
-			error: err
 		});
 
 		return;

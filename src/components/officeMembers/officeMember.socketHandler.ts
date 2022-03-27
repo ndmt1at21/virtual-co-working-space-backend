0154a99542a7b4293f070d9eea8b7a88aafda67f
ndmt1at21 <trminh36@gmail.com>
2022-03-27 22:30:57 +0700

    fix: minor fixes

--- src/components/officeMembers/officeMember.socketHandler.ts
+++ src/components/officeMembers/officeMember.socketHandler.ts
@@ -47,10 +47,11 @@ export const OfficeMemberSocketHandler = (
 				officeId: officeMember.officeId
 			};
 
-			await disconnectExistSocketHasSameUserId(userId);
+			// await disconnectExistSocketHasSameUserId(userId);
 			emitMemberOnlineToOffice(officeMember.memberId, officeId);
 			setMemberInOfficeOnline(officeMember.memberId);
 		} catch (err: any) {
+			console.log('errr:::', err);
 			socket.emit('office_member:error', err);
 			socket.disconnect();
 		}

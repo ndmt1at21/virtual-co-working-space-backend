4fbd724cfa34a408aea4e6fc21f68c9923b71e5f
vominhtrieu <minhtrieuvo600@gmail.com>
2022-05-06 23:37:07 +0700

    Video call feature

--- src/components/authSocket/authSocket.factory.ts
+++ src/components/authSocket/authSocket.factory.ts
@@ -2,11 +2,12 @@ import { createAuthValidate } from '../auth/auth.factory';
 import { createAuthTokenService } from '../auth/components/authToken/authToken.factory';
 import { createUserRepository } from '../users/user.factory';
 import { AuthSocketMiddleware } from './authSocket.middleware';
+import { ILogger } from '@components/logger/@types/ILogger';
 
-export function createSocketMiddleware() {
+export function createSocketMiddleware(logger: ILogger) {
 	const userRepository = createUserRepository();
 	const authTokenService = createAuthTokenService();
 	const authValidate = createAuthValidate();
 
-	return AuthSocketMiddleware(userRepository, authTokenService, authValidate);
+	return AuthSocketMiddleware(userRepository, authTokenService, authValidate, logger);
 }

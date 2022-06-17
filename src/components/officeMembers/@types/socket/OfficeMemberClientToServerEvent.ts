8957412a4ad93f893b3b5bec9b83c05eec60c1b7
vominhtrieu <minhtrieuvo600@gmail.com>
2022-06-17 18:42:24 +0700

    Add action event for socket

--- src/components/officeMembers/@types/socket/OfficeMemberClientToServerEvent.ts
+++ src/components/officeMembers/@types/socket/OfficeMemberClientToServerEvent.ts
@@ -1,8 +1,9 @@
 import { UpdateOfficeMemberTransformDto } from '@src/components/officeMemberTransform/@types/dto/UpdateOfficeMemberTransform';
+import { ActionListenerData } from '@src/components/offices/@types/dto/ActionData';
 import { JoinToOfficeRoomDto } from '@src/components/offices/@types/dto/JoinToOfficeRoom.dto';
 
 export interface OfficeMemberClientToServerEvent {
 	'office_member:join': (data: JoinToOfficeRoomDto) => void;
-
 	'office_member:move': (transform: UpdateOfficeMemberTransformDto) => void;
+	'office_member:action': (data: ActionListenerData) => void;
 }
--- src/components/offices/@types/OfficeServerToClientEvent.ts
+++ src/components/offices/@types/OfficeServerToClientEvent.ts
@@ -3,6 +3,7 @@ import { MessageServerToClientEvent } from '@src/components/messages/@types/Mess
 import { OfficeItemServerToClientEvent } from '@src/components/officeItems/@types/OfficeItemServerToClientEvent';
 import { OfficeMemberServerToClientEvent } from '@src/components/officeMembers/@types/socket/OfficeMemberServerToClientEvent';
 import { EmojiResponseData } from './dto/EmojiData';
+import { ActionResponseData } from './dto/ActionData';
 
 export interface OfficeServerToClientEvent
 	extends OfficeMemberServerToClientEvent,
@@ -11,4 +12,5 @@ export interface OfficeServerToClientEvent
 	'office:error': (err: any) => void;
 	emoji: (data: EmojiResponseData) => void;
 	gesture: (data: GestureResponseData) => void;
+	action: (data: ActionResponseData) => void;
 }
